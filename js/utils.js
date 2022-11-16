3080c5e554fc25d12991554a3303b70a6dbb3e20
anastl <anna.207346@gmail.com>
2022-11-16 15:23:10 -0400

    las touches to one column css + mockup functions added

--- js/utils.js
+++ js/utils.js
@@ -144,4 +144,79 @@ function displayHeaderAndSearchBar() {
     `)
 }
 
-export { displayLogin, displayHeaderAndSearchBar, Movie }
+function resultMockup() {
+    return `
+    <div class="movie"
+        style="
+            background: linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .8)), url('../../Not for github/pixel perfect img/Avatar_1.png');
+            background-position: center;
+            background-size: cover;
+    ">
+        <div class="stars-and-genre">
+            <p class="genre blue">${ 'Science Fiction' }</p>
+            <div class="stars-container">
+                <img class="star" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" />
+                <img class="star" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" />
+                <img class="star" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" />
+                <img class="star" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" />
+                <img class="star" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" />
+            </div>
+        </div>
+        <p class="text--bold movie-title">${ 'Avatar 2' }</p>
+        <p class="summary">${ 'Set more than a decade after the events of the first film, Avatar: The Way of Water begins to tell the story of the Sully family, the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.' }</p>
+        <button class="watch-now" id="watch-movie">Watch Now</button>
+    </div>
+    `
+}
+
+function mostWatchedMockup() {
+    return (`
+    <div class="most-watched-movies">
+        <div class="mw"
+            style=" background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 100%), url('../../Not for github/pixel perfect img/Prey_mw.png'); background-position: center; background-size: cover;"
+        >
+            <div class="details--mw">
+                <p class="text--bold movie-title mw-title">${ 'Prey' }</p>
+                <div class="stars-container stars-container--mw">
+                    <img class="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" /> <img class="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" /> <img class ="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" /> <img class ="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" /> <img class ="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" />
+                </div>
+                <p class="summary summary--mw">${ 'In 1719 in the Great Plains, Naru, a young Comanche woman trained as a healer, dreams of becoming a great hunter.' }</p>
+            </div>
+        </div>
+        <div class="mw"
+            style=" background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 100%), url('../../Not for github/pixel perfect img/Jurassic_mw.png'); background-position: center; background-size: cover;"
+        >
+            <div class="details--mw">
+                <p class="text--bold movie-title mw-title">${ 'Jurassic World Dominion' }</p>
+                <div class="stars-container stars-container--mw">
+                    <img class="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" /> <img class="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" /> <img class ="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" /> <img class ="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" /> <img class ="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" />
+                </div>
+                <p class="summary summary--mw">${ 'Four years after the Lockwood Estate incident and the cataclysmic volcanic eruption on Isla Nublar.' }</p>
+            </div>
+        </div>
+        <div class="mw"
+            style=" background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 100%), url('../../Not for github/pixel perfect img/Kong_mw.png'); background-position: center; background-size: cover;"
+        >
+            <div class="details--mw">
+                <p class="text--bold movie-title mw-title">${ 'Godzilla vs Kong' }</p>
+                <div class="stars-container stars-container--mw">
+                    <img class="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" /> <img class="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" /> <img class ="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" /> <img class ="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" /> <img class ="star star--mw" src="./img/vectors/star.png" alt="star rating" aria-hidden="true" />
+                </div>
+                <p class="summary summary--mw">${ 'Five years after Godzilla defeated King Ghidorah, Kong is monitored by Monarch within a giant dome on Skull Island. ' }</p>
+            </div>
+        </div>
+    </div>
+    `)
+}
+
+function homeMockup() {
+    return (`
+        ${ resultMockup() }
+        <div class="most-watched-container">
+            <p class="title title--mw text--bold">Most Watched Movies</p>
+            ${ mostWatchedMockup() }
+        </div>
+    `)
+}
+
+export { displayLogin, displayHeaderAndSearchBar, Movie, homeMockup }
