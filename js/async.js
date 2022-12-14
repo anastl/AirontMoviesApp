835deb326071bfab81bed7831078f3683e1ce7e9
anastl <anna.207346@gmail.com>
2022-12-13 23:04:32 -0400

    refactored addSimilarMovies to store similarMovies in sessionStorage

--- js/async.js
+++ js/async.js
@@ -11,6 +11,89 @@ import {
     selectViewCallback
 } from './utils.js'
 
+async function addMoviesToMostWatched( lastPage, observer ) {
+    try {
+        const { movies, totalPages } = await fetchMostWatched( lastPage + 1 )
+        
+        const mostWatchedArray = movies.map( ( { id, title, overview, vote_average, backdrop_path } ) => {
+            if ( id && title && overview && vote_average && backdrop_path ) { 
+                return asMostWatched( id, title, overview, vote_average, backdrop_path ) 
+            }
+        } ).join('')
+            
+        const mostWatchedContainer = document.getElementById('most-watched--results')
+        mostWatchedContainer.insertAdjacentHTML( 'beforeend', mostWatchedArray )
+    
+        const trigger = document.getElementById('trigger')
+        trigger.removeAttribute('id')
+        
+        const targetArray = document.getElementsByClassName('most-watched--movie')
+        const target = targetArray[ targetArray.length - 10 ]
+        target.setAttribute('id', 'trigger')
+        target.dataset.lastPage = parseInt(lastPage) + 1
+        target.dataset.totalPages = totalPages
+        observer.observe( target )
+    } catch ( e ) { console.log( e ) }
+}
+
+async function addSimilarMovies() { 
+    const addMoreBtn = document.getElementById('add-more')
+    
+    const movieId = addMoreBtn.dataset.movieId
+    const oldPage = parseInt( addMoreBtn.dataset.page )
+    const oldNumberOfRecs = parseInt( addMoreBtn.dataset.numberOfRecs )
+
+    const newNumberOfRecs = oldNumberOfRecs+3
+    const isEndOfCurrPage = newNumberOfRecs >= oldPage*18 ? true : false
+    const newPage = isEndOfCurrPage ? oldPage+1 : oldPage
+
+    let memoryRecs = JSON.parse( sessionStorage.getItem('similarMovies') )
+    if ( ! memoryRecs ) { // first time fetching similar movies 
+        memoryRecs = await fetchSimilar( movieId, newPage )
+        sessionStorage.setItem('similarMovies', JSON.stringify( memoryRecs ) )
+    } 
+    else if ( isEndOfCurrPage ) { // time to get a new page from API
+        memoryRecs = [ ...memoryRecs, ...await fetchSimilar( movieId, newPage ) ]
+        sessionStorage.setItem('similarMovies', JSON.stringify( memoryRecs ) )
+    }
+
+    const recommendedImgs = memoryRecs.map( ( { id, title, backdrop_path } ) => asRecommended( id, title, backdrop_path ) ).slice( oldNumberOfRecs, newNumberOfRecs ).join('')
+    addMoreBtn.insertAdjacentHTML( 'beforebegin', recommendedImgs )
+    addMoreBtn.dataset.page = newPage
+    addMoreBtn.dataset.numberOfRecs = newNumberOfRecs
+
+    const showModalArray = [ ...document.getElementsByClassName('sm') ]
+    showModalArray.forEach( el => setModalOpener( el ) )
+
+    addMoreBtn.scrollIntoView( { behavior: 'smooth' } )
+} 
+
+async function displayModal( movieId ) {
+    sessionStorage.removeItem('similarMovies')
+    const modalContainer = document.getElementById('modal-container')
+
+    const { id, title, genres, overview, release_date, backdrop_path, original_language, vote_average } = await fetchMovie( movieId )
+
+    modalContainer.innerHTML = asModal( id, title, genres[0], overview, release_date, backdrop_path, original_language, vote_average )
+
+    modalContainer.style.display = 'flex'
+    document.getElementById('most-watched-container').style.display = 'none'
+    document.getElementById('results-container').style.display = 'none'
+
+    document.getElementById('close-modal').addEventListener('click', () => {
+        modalContainer.style.display = 'none'
+        modalContainer.innerHTML = ''
+        document.getElementById('most-watched-container').style.display = 'flex'
+        document.getElementById('results-container').style.display = 'flex'
+        sessionStorage.removeItem('similarMovies')
+    } )
+
+    const addMoreBtn = document.getElementById('add-more')
+    addMoreBtn.addEventListener( 'click', addSimilarMovies )
+
+    await addSimilarMovies( )
+}
+
 async function fetchGenres() {
     try {
         const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=a549a10218e6b1e84fddfc056a830b2c`
@@ -29,15 +112,6 @@ async function fetchLanguages() {
     } catch ( e ) { console.log( e ) }
 }
 
-async function fetchMovie( movieId ) {
-    try {
-        const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=a549a10218e6b1e84fddfc056a830b2c&language=en-US`
-        const res = await fetch( url )
-        const movie = await res.json()
-        return movie
-    } catch ( e ) { console.log( e ) }
-}
-
 async function fetchMostWatched( page ) {
     try {
         const url = `https://api.themoviedb.org/3/movie/popular?api_key=a549a10218e6b1e84fddfc056a830b2c&language=en-US&page=${ parseInt(page) }`
@@ -50,22 +124,34 @@ async function fetchMostWatched( page ) {
     } catch ( e ) { console.log( e ) }
 }
 
+async function fetchMovie( movieId ) {
+    try {
+        const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=a549a10218e6b1e84fddfc056a830b2c&language=en-US`
+        const res = await fetch( url )
+        const movie = await res.json()
+        return movie
+    } catch ( e ) { console.log( e ) }
+}
+
 async function fetchSimilar( movieId, page ){
     try {
         const baseUrl = `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=a549a10218e6b1e84fddfc056a830b2c&language=en-US&page=${page}`
         const res = await fetch( baseUrl )
         const recommended = await res.json()
-        if ( recommended.total_results ) {
-            const recommendedArray = recommended.results
-            const cleanSimilar = recommendedArray.filter( ( { id } ) => id != movieId )
+        
+        if ( recommended.total_pages === page )
+            document.getElementById('add-more').style.display = 'none'
 
-            const completeSimilar = []
-            cleanSimilar.forEach( ( { id, title, backdrop_path } ) =>{
-                if( id && title && backdrop_path )
-                    completeSimilar.push( { id, title, backdrop_path } )
+        else if ( recommended.total_results ) {
+            const recommendedArray = recommended.results
+            const cleanSimilar = []
+            recommendedArray.forEach( ( { id, title, backdrop_path } ) =>{
+                // console.log( id != movieId, id, movieId )
+                if( id != movieId && title && backdrop_path )
+                    cleanSimilar.push( { id, title, backdrop_path } )
             } )
 
-            return completeSimilar
+            return cleanSimilar
         } else {
             document.getElementById('similar-container').style = 'none'
             document.getElementById('similar-container--title').style = 'none'
@@ -93,105 +179,11 @@ async function getUser( email, password ){
     }
 }
 
-async function searchMovieAsMain ( query ) {
-    try {
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=a549a10218e6b1e84fddfc056a830b2c&language=en-US&query=${ query }&include_adult=false`
-        const res = await fetch( url ) 
-        const searchResults = await res.json()
-
-        const moviesArray = searchResults.results.slice(0,5).map( ( { id, title, genre_ids, vote_average, overview, backdrop_path } ) => asMain( id, title, genre_ids[0], overview, vote_average, backdrop_path ) ).join(' ')
-
-        return moviesArray
-    } catch ( e ) { document.getElementById('error-modal').insertAdjacentHTML( 'beforeend', asError( e ) ) }
-}
-
-async function searchMovieAsDropdown ( query ) {
-    try {
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=a549a10218e6b1e84fddfc056a830b2c&language=en-US&query=${ query }&include_adult=false`
-        const res = await fetch( url )
-        const searchResults = await res.json()
-        if ( searchResults.total_results ) {
-            const moviesArray = searchResults.results.slice(0,5).map( ( { id, title, release_date, poster_path } ) => asDropdown( id, title, release_date, poster_path ) ).join(' ')
-    
-            return moviesArray
-        } else {
-            document.getElementById('dropdown-container').style.display = 'none'
-        }
-
-    } catch ( e ) { document.getElementById('error-modal').insertAdjacentHTML( 'beforeend', asError( e ) ) }
-}
-
-async function addMoviesToMostWatched( lastPage, observer ) {
-    try {
-        const { movies, totalPages } = await fetchMostWatched( lastPage + 1 )
-        
-        const mostWatchedArray = movies.map( ( { id, title, overview, vote_average, backdrop_path } ) => {
-            if ( id && title && overview && vote_average && backdrop_path ) { 
-                return asMostWatched( id, title, overview, vote_average, backdrop_path ) 
-            }
-        } ).join('')
-            
-        const mostWatchedContainer = document.getElementById('most-watched--results')
-        mostWatchedContainer.insertAdjacentHTML( 'beforeend', mostWatchedArray )
-    
-        const trigger = document.getElementById('trigger')
-        trigger.removeAttribute('id')
-        
-        const targetArray = document.getElementsByClassName('most-watched--movie')
-        const target = targetArray[ targetArray.length - 10 ]
-        target.setAttribute('id', 'trigger')
-        target.dataset.lastPage = parseInt(lastPage) + 1
-        target.dataset.totalPages = totalPages
-        observer.observe( target )
-    } catch ( e ) { console.log( e ) }
-}
-
-// SAVE API CALLS TO SESSION STORAGE
-async function addSimilarMovies( movieId ) { //handle modal, works
-
-    const addMoreBtn = document.getElementById('add-more')
-    const oldPage = parseInt( addMoreBtn.dataset.page )
-    const recommendedArray = await fetchSimilar( movieId, oldPage )
-
-    if ( oldPage === 1 ) {
-        sessionStorage.setItem('similarMovies', JSON.stringify( recommendedArray ) )
-    } 
-    
-    const recommendedImgs = recommendedArray.map( ( { id, title, backdrop_path } ) => asRecommended( id, title, backdrop_path ) )
-
-    const parent = addMoreBtn.parentElement
-    
-    recommendedImgs.slice( 0, 3 ).forEach( imgDiv => parent.insertBefore( imgDiv, addMoreBtn ) )
-    
-    addMoreBtn.addEventListener('click', async () => {
-        const oldIndex = parseInt( addMoreBtn.dataset.numberOfRecs )
-        const newIndex = oldIndex + 3
-        const page = parseInt( addMoreBtn.dataset.page )
-        const numberOfSimilar = 18 * parseInt( page ) 
-        addMoreBtn.dataset.numberOfRecs = newIndex
-        addMoreBtn.dataset.page = newIndex >= numberOfSimilar ? parseInt( page )+1 : page
-        if( newIndex >= numberOfSimilar  ) {
-            const moreSimilar = await fetchSimilar( movieId, parseInt( page )+1 )
-            moreSimilar.forEach( img => recommendedArray.push( img ) )
-            sessionStorage.setItem('similarMovies', JSON.stringify( recommendedArray ) )
-        }
-
-        const recImgs = recommendedArray.map( ( { id, title, backdrop_path } ) => asRecommended( id, title, backdrop_path ) )
-
-        recImgs.slice( oldIndex, newIndex ).forEach( imgDiv => parent.insertBefore( imgDiv, addMoreBtn ) )
-
-        const showModalArray = [ ... document.getElementsByClassName('sm') ]
-    showModalArray.forEach( el => {
-        setModalOpener( el )
-    } )
-    } )
-} 
-
 async function searchInputCallback( e ) {
     const searchMovieInput = document.getElementById('search-movie')
     const query = searchMovieInput.value
     const dropdown = document.getElementById('dropdown-container')
-    const mainContainer = document.getElementById('results-container')
+    const resultsContainer = document.getElementById('results-container')
 
     if ( ! query || query.length === 0 ) dropdown.style.display = 'none' 
     if ( query.length < 3 ) return
@@ -204,24 +196,54 @@ async function searchInputCallback( e ) {
         } else {
             sessionStorage.setItem('lastSearches', JSON.stringify( [ searchMovieInput.value ] ) )
         }
-        mainContainer.insertAdjacentHTML( 'beforeend', await searchMovieAsMain( query ) )
-        dropdown.insertAdjacentHTML( 'beforeend', '' )
+        resultsContainer.innerHTML =  await searchMovieAsMain( query )
+        dropdown.innerHTML = ''
         searchMovieInput.value = ''
         dropdown.style.display = 'none'
 
         const showModalArray = [ ... document.getElementsByClassName('sm') ]
         showModalArray.forEach( el => setModalOpener( el ) )
     } else { // Show dropdown
-        dropdown.innerHTML = ''
-        dropdown.insertAdjacentHTML( 'beforeend', await searchMovieAsDropdown( query ) )
-
+        dropdown.innerHTML = await searchMovieAsDropdown( query ) 
         const showModalArray = [ ... document.getElementsByClassName('sm') ]
         showModalArray.forEach( el => setModalOpener( el ) )
     }
 }
 
+async function searchMovieAsDropdown ( query ) {
+    try {
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=a549a10218e6b1e84fddfc056a830b2c&language=en-US&query=${ query }&include_adult=false`
+        const res = await fetch( url )
+        const searchResults = await res.json()
+        if ( searchResults.total_results ) {
+            const moviesArray = []
+            searchResults.results.slice(0,5).forEach( ( { id, title, release_date, poster_path } ) => {
+                if ( id && title && release_date && poster_path )
+                    moviesArray.push( asDropdown( id, title, release_date, poster_path ) )
+            }  )
+    
+            return moviesArray.join(' ')
+        } else {
+            document.getElementById('dropdown-container').style.display = 'none'
+        }
+
+    } catch ( e ) { document.getElementById('error-modal').innerHTML = asError( e ) }
+}
+
+async function searchMovieAsMain ( query ) {
+    try {
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=a549a10218e6b1e84fddfc056a830b2c&language=en-US&query=${ query }&include_adult=false`
+        const res = await fetch( url ) 
+        const searchResults = await res.json()
+
+        const moviesArray = searchResults.results.slice(0,5).map( ( { id, title, genre_ids, vote_average, overview, backdrop_path } ) => asMain( id, title, genre_ids[0], overview, vote_average, backdrop_path ) ).join(' ')
+
+        return moviesArray
+    } catch ( e ) { document.getElementById('error-modal').innerHTML = asError( e ) }
+}
+
 async function setUpHome() {
-    document.getElementById('master-container').insertAdjacentHTML( 'beforeend', homeHtml ) 
+    document.getElementById('master-container').innerHTML = homeHtml
         
     const { movies, totalPages } = await fetchMostWatched( 1 )
 
@@ -234,11 +256,11 @@ async function setUpHome() {
     const { id, title, genre_ids, overview, vote_average, backdrop_path } = movies[0]
     const mainMovie = asMain( id, title, genre_ids[0], overview, vote_average, backdrop_path )
 
-    const mainContainer = document.getElementById('results-container')
+    const resultsContainer = document.getElementById('results-container')
     const mostWatchedContainer = document.getElementById('most-watched--results')
 
-    mainContainer.insertAdjacentHTML( 'beforeend', mainMovie ) 
-    mostWatchedContainer.insertAdjacentHTML( 'beforeend', mostWatchedArray ) 
+    resultsContainer.innerHTML = mainMovie 
+    mostWatchedContainer.innerHTML = mostWatchedArray 
 
     const targetArray = mostWatchedContainer.getElementsByClassName('most-watched--movie')
     const target = targetArray[ targetArray.length - 10 ]
@@ -270,7 +292,7 @@ async function setUpHome() {
     } )
     
     document.addEventListener('click', event => {
-        const dropdown = document.getElementById('dropdown-container');
+        const dropdown = document.getElementById('dropdown-container')
       
         if (!dropdown.contains(event.target)) {
           dropdown.style.display = 'none'
@@ -285,28 +307,35 @@ async function setUpHome() {
     viewBtns.forEach( btn => btn.addEventListener('click', selectViewCallback) )
 }
 
-async function displayModal( movieId ) {
-    const modalContainer = document.getElementById('modal-container')
 
-    const { id, title, genres, overview, release_date, backdrop_path, original_language, vote_average } = await fetchMovie( movieId )
+// async function recommendedMoviesCallback( ) {
+//     console.log( this )
+//     const movieId = this.dataset.movieId
+//     const oldPage = parseInt( this.dataset.page )
 
-    modalContainer.innerHTML = asModal( id, title, genres[0], overview, release_date, backdrop_path, original_language, vote_average )
+//     const recommendedArray = await fetchSimilar( movieId, oldPage )
 
-    modalContainer.style.display = 'flex'
-    document.getElementById('most-watched-container').style.display = 'none'
-    document.getElementById('results-container').style.display = 'none'
+//     const oldIndex = parseInt( this.dataset.numberOfRecs )
+//     const newIndex = oldIndex + 3
+//     const page = parseInt( this.dataset.page )
+//     const numberOfSimilar = 18 * parseInt( page ) 
+//     this.dataset.numberOfRecs = newIndex
+//     this.dataset.page = newIndex >= numberOfSimilar ? parseInt( page )+1 : page
+//     if( newIndex >= numberOfSimilar  ) {
+//         const moreSimilar = await fetchSimilar( movieId, parseInt( page )+1 )
+//         moreSimilar.forEach( img => recommendedArray.push( img ) )
+//         sessionStorage.setItem('similarMovies', JSON.stringify( recommendedArray ) )
+//     }
 
-    document.getElementById('close-modal').addEventListener('click', () => {
-        modalContainer.style.display = 'none'
-        modalContainer.innerHTML = ''
-        document.getElementById('most-watched-container').style.display = 'flex'
-        document.getElementById('results-container').style.display = 'flex'
-    } )
-    await addSimilarMovies( movieId )
+//     const recImgs = recommendedArray.map( ( { id, title, backdrop_path } ) => asRecommended( id, title, backdrop_path ) )
 
-    const showModalArray = [ ... document.getElementsByClassName('sm') ]
-    showModalArray.forEach( el => setModalOpener( el ) )
-}
+//     recImgs.slice( oldIndex, newIndex ).forEach( imgDiv => this.parentElement.insertBefore( imgDiv, this ) )
+
+//     const showModalArray = [ ... document.getElementsByClassName('sm') ]
+//     showModalArray.forEach( el => {
+//         setModalOpener( el )
+//     } )
+// }
 
 export {
     fetchGenres, 
